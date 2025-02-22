8a328275e63eed1280a7a86c5728884b83886435
Harsh Sharma <harshusharma9896@gmail.com>
2025-02-22 18:07:57 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
+#!/bin/zsh
+
+# Move the zsh configuration directory (requires sudo)
 sudo mv zsh /usr/share/zsh
 
+# Move the config files to the home directory
 mv .zshrc ~/.zshrc
-
 mv .p10k.zsh ~/.p10k.zsh
 
-source ~/.zshrc
+# Source the new configuration
 source ~/.p10k.zsh
+source ~/.zshrc
