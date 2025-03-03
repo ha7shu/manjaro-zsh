e04de8a1ae8efaad4a65a710a13f6f7460220b3c
Harsh Sharma <harshusharma9896@gmail.com>
2025-03-03 18:35:47 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # Move the zsh configuration directory (requires sudo)
 sudo mv /usr/share/zsh /usr/share/zsh_backup
-sudo mv zsh /usr/share/
+sudo mv ./zsh /usr/share/
 
 # Move the config files to the home directory
 mv -i .zshrc ~/.zshrc
